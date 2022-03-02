95780690090ec9457a3a9a495387b468ebb492dc
Alex Henner <jahenner@gmail.com>
2022-03-01 22:17:49 -0500

    I did too much before commiting again...

--- exercise_ui/exercise_ui/src/App.js
+++ exercise_ui/exercise_ui/src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, { useState} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AddPage from './pages/CreatePage';
 import EditPage from './pages/EditPage';
 
 function App() {
+  const [exerciseToEdit, setExerciseToEdit] = useState();
   return (
     <div className="App">
       <Router>
         <div className="App-header">
           <Route path="/" exact>
-            <HomePage />
+            <HomePage setExerciseToEdit={setExerciseToEdit} />
           </Route>
-          {/* <Route path="/add-exercise">
+          <Route path="/add-exercise">
             <AddPage />
           </Route>
           <Route path="/edit-exercise">
-            <EditPage />
-          </Route> */}
+            <EditPage exerciseToEdit={exerciseToEdit} />
+          </Route>
         </div>
       </Router>
     </div>
